e5e088d07dd9988056b9d2ee9428573de0c3d140
Daniel Ben Porat <daniel.benporat@post.runi.ac.il>
2025-12-17 14:43:08 +0200

    Documenting_Editor4

--- Editor4.java
+++ Editor4.java
@@ -1,5 +1,13 @@
 import java.awt.Color;
-
+/**
+ * Demonstrates the morphing operation featured by Runigram.java to grayscale of the same image. 
+ * The program recieves two command-line arguments: a string representing the name
+ * of the PPM file image and the number of morphing steps (an int). 
+ * For example, to morph the cake into grayscale cake in 50 steps, use:
+ * java Editor3 cake.ppm 50
+ * Note: There is no need to scale the target image to the size of the source
+ * image, since Runigram.morph performs this action.
+ */
 public class Editor4 {
     public static void main (String[] args) {
 		String fileName = args[0];
